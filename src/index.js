f0a4ce559ea7badfad580b9577de5166a48eed56
yuna <elldia1222@gmail.com>
2024-09-01 14:17:17 +0900

    충돌 수정 240901

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-<<<<<<< HEAD
-import { Provider } from 'react-redux';
-import store from './store';
-=======
 import store from './Store'; // redux
 import { Provider } from 'react-redux';
->>>>>>> main
 
 const queryClient = new QueryClient();
 
