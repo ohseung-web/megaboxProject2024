5145aa6566b317988b530b242459cd6297561f58
kjb1045 <kjb1045@naver.com>
2024-09-23 18:27:03 +0900

    comfirm 취소버튼 클릭 에러 수정

--- src/pages/Reservation/Reservation.jsx
+++ src/pages/Reservation/Reservation.jsx
@@ -4,6 +4,7 @@ import './../../common/Common.css';
 import reset from './images/ico-reset-small.png';
 import impossible from './images/bg-seat-condition-impossible.png';
 import choice from './images/bg-seat-condition-choice.png';
+import common from './images/bg-seat-condition-common.png';
 import ReserationInfo from './ReserationInfo';
 import { current } from '@reduxjs/toolkit';
 import { useSelector, useDispatch } from 'react-redux';
@@ -52,8 +53,11 @@ const Reservation = () => {
   // ReservationModal창의 messg내용을 저장하는 변수 예) 1,2,3,4,5,6... 번호마다 메시지 내용이 다름
   const [msg,setMsg] = useState(0);
   // 예매인원의 이전상태를 저쟝하는 변수
-  const [prevState,setPrevState] =useState(null);
-  const [seatCount02, setSeatCount02] = useState(0); // 적절한 초기값 설정
+ // const [prevState,setPrevState] =useState(null);
+  // 선택한 예매좌석의 개수를 저장하는 변수
+ // const [selectSeatCount, setselectSeatCount] = useState(0); 
+  // 선택한 예매인원의 최초 인원수를 저장하는 변수
+  const [selectGroupCount, setSelectGroupCount] = useState([])
 
   // 전체 예매 인원수 계산하는 함수
   const seatTableCheckHandler = () => {
@@ -69,12 +73,13 @@ const Reservation = () => {
       seatReset();
   }),[])
 
-   // 초기화버튼 클릭하면 선택좌석 selectSeat의 배열초기화, 총예매인원수=0, 총금액=0, 선택한인원구분='' 초기화 하는 함수
+   // 초기화버튼 클릭하면 선택좌석 selectSeat의 배열초기화, 총예매인원수=0, 총금액=0, 선택한인원구분='' 선택한 전체 예매인원수 초기화 하는 함수
    const seatReset = () => {
     dispatch(reSet());
     setSelectSeat([]);
     setTotalPrice(0);
     setChoicePeople('');
+    setSelectGroupCount([]);
   };
 
   //Modal 라이브러리 스타일 함수
@@ -95,7 +100,7 @@ const Reservation = () => {
       // border: '3px solid #111',
     },
   };
-   //모달창이 화면에 출력될 때 body의 스크롤바를 감추는 useEffect()가 실행된다.
+   //모달창이 화면에 출력될 때 body의 스크롤바를 감추는 useEffect()
     useEffect((()=>{
       if(modalOpen){
          document.body.style.overflow = 'hidden'
@@ -106,6 +111,14 @@ const Reservation = () => {
           document.body.style.overflow ='auto'
       }
    }),[modalOpen])
+
+  let seatCount = selectSeat.length; // 선택한 좌석수
+  let adultCount = state.countList.items[0].count; // 선택한 성인 인원수
+  let teenagerCount = state.countList.items[1].count; //선택한 청소년 인원수
+  let childernCount = state.countList.items[2].count; //선택한 어린이 인원수
+  let seniorCount = state.countList.items[3].count; // 선택한 경로 인원수
+  let spacialCount = state.countList.items[4].count; // 선택한 우대 인원수
+  let totalSelectCount = 0; // adultCount+teenagerCount+childernCount+seniorCount+spacialCount
 
   // 클릭한 좌석의 행번호, 열번호를 selectSeat의 배열에 저장하는 함수
   // some 메서드는 배열의 각 요소를 순회하면서 제공된 조건 함수가 하나라도 true를 반환하면 true를 반환한다.
@@ -124,6 +137,13 @@ const Reservation = () => {
      // alert('선택할 수 없는 좌석입니다.');
       return ;
     }
+     // confirm에서 취소버튼을 클릭한 상태에서 다른 좌석을 선택할 때 modal출력 됨
+     if(totalSelectCount < selectGroupCount.length){
+      setmodalOpen(true)
+      setMsg(3)
+      return ;
+    } 
+
     // 좌석 선택 가능 여부 확인
     setSelectSeat((prev) => {
       //좌석이 선택되지 않으면  무조건 false 출력, 좌석이 선택이 되고 난후 true 출력
@@ -137,8 +157,9 @@ const Reservation = () => {
           (seat) => !(seat.rowIndex === rowIndex && seat.colIndex === colIndex)
         );
       } else {
+        
         //좌석이 선택되지 않은경우, 좌석추가
-        if (seatTableTotalcount.current > prev.length) {
+        if (seatTableTotalcount.current > prev.length ) {
           return [...prev, { rowIndex, colIndex }];
         } else {
           // 선택한 좌석의 수가 전체인원수 보다 크면 좌석추가 하지 않음
@@ -146,20 +167,11 @@ const Reservation = () => {
           setMsg(3)
          // alert('이미 좌석을 모두 선택하였습니다.');
           return prev;
-        }
-      }
+        } 
+      } 
     });
-    // choiceCountHandler();
-    // totalPriceHandler();
-    // choicePeopleHandler();
   };
 
-  let seatCount = selectSeat.length; // 선택한 좌석수
-  let adultCount = state.countList.items[0].count; // 선택한 성인 인원수
-  let teenagerCount = state.countList.items[1].count; //선택한 청소년 인원수
-  let childernCount = state.countList.items[2].count; //선택한 어린이 인원수
-  let seniorCount = state.countList.items[3].count; // 선택한 경로 인원수
-  let spacialCount = state.countList.items[4].count; // 선택한 우대 인원수
  // 좌석을 선택할때 일어나는 함수들 모음 ============================================================================
     // 선택한 좌석의 selectSeat (행번호, 열번호) 콘솔출력
     console.log(
@@ -176,7 +188,7 @@ const Reservation = () => {
       });
       setChoiceSeatNumber(choiceSeatNumber);
     };
-
+   
     // 예매인원 선택우선 순위 좌석 클릭 할때마다 예매정보창에 예매인원수 증가하는 함수
     const choiceCountHandler = () => {
       let  adult = 0,teenager = 0,childern = 0,senior = 0,spacial = 0
@@ -218,8 +230,9 @@ const Reservation = () => {
       setChoicePeople(people);
     };
     //==========================================================================================
-     // 예매좌석을 선택할때 마다 useEffect()가 실행된다.
+     // 예매좌석을 선택할때 마다 아래 함수들을 실행하는 useEffect()
     useEffect(() => {
+        //setselectSeatCount(seatCount);
         totalPriceHandler();
         choicePeopleHandler();
         choiceSeatDisply();
@@ -249,6 +262,11 @@ const Reservation = () => {
   }),[state.countList.msg])
 
   //---------------------------------------------------------------------------------------
+  // 이전상태를 관리하는 useEffect()
+  // useEffect(() =>{
+  //    setPrevState({ selectSeatCount, seatTableTotalcount:seatTableTotalcount.current })
+  // },[selectSeatCount])
+  
   // 전체예매인원과 선택한 좌석의 갯수가 같을 때 예매인원을 변경 시 window.confirm창 띄우는 함수
   const totalCountOver = () => {
     if (seatTableTotalcount.current === seatCount &&
@@ -256,25 +274,33 @@ const Reservation = () => {
         // confirm 대화상자
         const userConfirm = window.confirm('선택하신 좌석을 모두 취소하고 다시 선택하시겠습니까? ');
         if (userConfirm) {
+            //confirm의 확인버튼 클릭시 리셋 시킴
             seatReset();
         }else{
-           //confim에서 취소 버튼 클릭시 이전상태로 복원
-           if(prevState){
-               setSeatCount02(prevState.seatCount)
-               seatTableTotalcount.current = prevState.seatTableTotalcount
-           }
+           //confirm에서 취소 버튼 클릭시 이전에 선택한 예매인원을 selectGroupCount에 저장한다.
+           //이전 상태로 복원이 안되는 이유는 selectSeatCount를 UI로 연결하지 않았기때문이다.
+            setSelectGroupCount((prev)=>
+              [...prev,  adultCount, teenagerCount, childernCount, seniorCount, spacialCount ])
+            // selectGroupCount 총계
+            selectGroupCount.map((selectCount, i)=>{
+            totalSelectCount += selectCount[i]
+            })
         }
      }
+    
   }; 
+  console.log(
+    'confirm 취소 버튼 클릭 시 : ' +
+    selectGroupCount
+        .map((select) => select),
+  );
+  console.log("selectGroupCount : " + selectGroupCount.length)
  //---------------------------------------------------------------------------------------
   //minus, plus 버튼클릭 시 호출 되는 함수
   const handleButtonClick = (action,id) =>{
-        //이전 상태 저장
-        setPrevState({seatCount,seatTableTotalcount:seatTableTotalcount.current})
         dispatch(action(id));
         totalCountOver();
   }
- 
   //마우스 오버될때 배경이미지 변경하는 함수
   const hoverSeatEnter = (rowIndex, colIndex) => {
     setHoverSeat({ rowIndex, colIndex });
@@ -283,37 +309,46 @@ const Reservation = () => {
   const hoverSeatRemove = () => {
     setHoverSeat(null);
   };
+
   // 좌석 배경이미지 변경하는 함수
   const getSeatStyle = (rowIndex, colIndex) => {
-    // 예매인원이 1인경우 짝수열만 backgroundImage 변경하는 부분
+    // 총 예매인원이 1인경우 1명을 선택하고 난 후 impossible이미지로 변경되었던 짝수열이 원래의 회색이미지인  common이미지로 변경되는 부분
+    if(seatTableTotalcount.current === 1 && choiceSeatNumber.length !== 0 && colIndex % 2 !== 0 ){ 
+        return {
+          backgroundImage: `url(${common})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }; 
+      }
+    // 총 예매인원이 1인경우 짝수열만 background를 impossible이미지로 변경하는 부분
     if (seatTableTotalcount.current === 1 && colIndex % 2 !== 0) {
       return {
         backgroundImage: `url(${impossible})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
       };
-    } else if (seatTableTotalcount.current === 0) {
-      //예매인원이 0인 경우 마우스오버해도 배경이미지가 변경되지 않는 부분
-      return hoverSeat ? {} : {};
-    }
-    // 마우스를 오버하면 배경이미지 변경하고, 선택한 좌석의 배경이미지를 변경하는 부분
-    return hoverSeat &&
-      hoverSeat.rowIndex === rowIndex &&
-      hoverSeat.colIndex === colIndex
-      ? {
-          backgroundImage: `url(${choice})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-        }
-      : selectSeat.some(
-          (seat) => seat.rowIndex === rowIndex && seat.colIndex === colIndex
-        )
-      ? {
-          backgroundImage: `url(${choice})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-        }
-      : {};
+    }else if (seatTableTotalcount.current === 0) {
+        //예매인원이 0인 경우 마우스오버해도 배경이미지가 변경되지 않는 부분
+        return hoverSeat ? {} : {};
+      }
+      // 마우스를 오버하면 배경이미지 변경하고, 선택한 좌석의 배경이미지를 변경하는 부분
+      return hoverSeat &&
+        hoverSeat.rowIndex === rowIndex &&
+        hoverSeat.colIndex === colIndex
+        ? {
+            backgroundImage: `url(${choice})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }
+        : selectSeat.some(
+            (seat) => seat.rowIndex === rowIndex && seat.colIndex === colIndex
+          )
+        ? {
+            backgroundImage: `url(${choice})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }
+        : {};
   };
 
   // 모달 close 함수 ReservateionMoadal 컴포넌트의 매개변수로 보내기 위해 작성한 함수
@@ -362,7 +397,9 @@ const Reservation = () => {
                       >
                         -
                       </button>
-                      <label className="cnt">{state.countList.items[i].count}</label>
+                      <label className="cnt">{
+                      selectGroupCount.length === 0 ? state.countList.items[i].count : selectGroupCount[i] 
+                      }</label>
                       <button
                         className="plus"
                         onClick={() => handleButtonClick(plusCount,state.countList.items[i].id)}
