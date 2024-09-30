f90418b905dcb1ab269b1e01787b9942ccce377d
yuna <elldia1222@gmail.com>
2024-09-30 21:52:01 +0900

    0930 store

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ## megabox 2024 -by yuna :kissing_heart:
+## 실행 위한 npm 설치 명령어 모음
+* `npm install recoil`
+* `npm install @fortawesome/free-regular-svg-icons @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome @fortawesome/free-brands-svg-icons`
+* `npm install #reduxjs/toolkit react-redux`
+* `npm install react-router-dom`
+* `npm install react-modal`
+* ...npm 설치 필요 시 추가 예정 목록
+------
 ### gitHub & google Drive url
 * https://github.com/ohseung-web/megaboxProject2024.git
 * https://drive.google.com/drive/folders/1uEduYI6iV4O97hh_ENqxVRg9NQlzd9ZF?usp=sharing
--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Store2 from './Store2'; // redux
 import { Provider } from 'react-redux';
+import store from './store';
 
 const queryClient = new QueryClient()
 
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
-    <Provider  store={Store2}>
+    <Provider  store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
