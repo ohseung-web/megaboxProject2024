873dae9539228616a95328d772ea4b58177435ac
yuna <elldia1222@gmail.com>
2024-09-30 22:10:58 +0900

    store js rename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
-import store from './store';
+import Store from './Store';
 
 const queryClient = new QueryClient();
 
@@ -15,7 +15,7 @@ root.render(
   // <React.StrictMode>
   // <Provider store={store}>
   <QueryClientProvider client={queryClient}>
-    <Provider  store={store}>
+    <Provider  Store={Store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
