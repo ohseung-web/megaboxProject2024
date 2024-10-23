42078276ea03d253bada61beab83e7b9fdcf3c6f
yuna <elldia1222@gmail.com>
2024-10-23 10:20:35 +0900

    score 경로 및 파일명 변경

--- src/pages/Movies/TabContent/SingleMovie.jsx
+++ src/pages/Movies/TabContent/SingleMovie.jsx
@@ -9,7 +9,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHeart as faHeartBlank, faHeart, faPlus } from "@fortawesome/free-regular-svg-icons"
 //redux
 import { useDispatch } from "react-redux"
-import { addToMovies } from "../../../Store2"
+import { addToMovies } from "../../../store"
 
 export default function SingleMovie(){
     const {data, isLoading, isError, error} = useSingleMoviesQuery()
